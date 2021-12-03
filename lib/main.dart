b7c025dd11d6ade35e1422079c1da6649898fe6a
Pedro Avelar <pedro-curiel@hotmail.com>
2021-12-02 21:35:38 -0300

    init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'dart:math';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:projeto/Screens/Home/intheater_page.dart';
 import 'package:projeto/Screens/About/about.dart';
 import 'Screens/Home/comingsoon_page.dart';
+import 'Screens/Home/home_page.dart';
 import 'Screens/Recover/recover_password.dart';
 import 'Screens/Recover/find_account.dart';
 import 'Screens/Create_Account/create_account.dart';
@@ -9,7 +13,14 @@ import 'Screens/Welcome/welcome_page.dart';
 import 'Screens/Login/login_page.dart';
 
 
-void main() {
+Future<void> main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  const String email = "";
+  const String nome = "";
+
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -22,6 +33,7 @@ void main() {
         'find' : (context) => FindAccountPage(),
         'create_account' : (context) => CreateAccountPage(),
         'about' : (context) => AboutPage(),
+        'home' : (context) => HomePage(email: email, nome: nome),
         '/Nos Cinemas' : (context) => InTheaterPage(),
         '/Em Breve' : (context) => ComingSoonPage(),
       }
